414fdd20fe9cccc39a01f41c4ec34aa5b2a4ded6
Spaghedward <147555727+Spaghedward@users.noreply.github.com>
2024-04-08 17:05:05 -0400

    Update ResumePage.jsx

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Styles = styled.div`
+  padding-top: 125px;
+`
 
 function ResumePage() {
   return (
-    <div>
-      <h1>
-        Resume under construction.
-      </h1>
-    </div>
+    <Styles>
+      <h2>
+        Front-end Proficiencies
+      </h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>responsive Design</li>
+        <li>JQuery</li>
+        <li>React</li>
+        <li>Bootstrap</li>
+      </ul>
+      <h2>Back-end Proficiencies</h2>
+      <ul>
+        <li>APIs</li>
+        <li>Node</li>
+        <li>Express</li>
+        <li>MySql, Sequelize</li>
+        <li>Restful API</li>
+        <li>Graphql</li>
+      </ul>
+    </Styles>
   );
 }
 
