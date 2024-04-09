7c2e86bfa2187aa77264ae12ba5579865542f2f2
Gordon <gordonjackman@gmail.com>
2024-04-09 11:34:04 -0400

    readme done.

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # IwaslikePortfolio
+
+![Static Badge](https://img.shields.io/badge/License-MIT-blue?style=plastic)
+
+[GrodenDesign](https://cerulean-lebkuchen-3eda2c.netlify.app/)
+
+## Table of Contents
+  * [Photo](#photo)
+  * [Description](#description)
+  * [Future Development](#future-development)
+  * [Collaboration](#collaboration)
+  * [Installation](#installation)
+  * [Contact](#contact)
+
+## Photo
+![Picture of App](image.png)
+
+## Description
+This is the webpage for my projects.
+
+## Future Development
+* Adding more advanced projects to the portfolio.
+* Finishing the contact card.
+
+## Collaboration
+Feel free to use this as you please.  
+
+## Installation
+* For installation you must `npm init`.
+* Use `npm install` to install dependencies.
+* Use `npm start` to start the server, or deploy to heroku or render etc.
+
+## Contact
+[Github](https://github.com/Spaghedward)
--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,12 +1,48 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Styles = styled.div`
+  padding-top: 125px;
+
+`
 
 function ResumePage() {
   return (
-    <div>
-      <h1>
-        Resume under construction.
-      </h1>
-    </div>
+    <Styles>
+      <h2>
+        Front-end Proficiencies
+      </h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>responsive Design</li>
+        <li>JQuery</li>
+        <li>React</li>
+        <li>Bootstrap</li>
+      </ul>
+      <h2>Back-end Proficiencies</h2>
+      <ul>
+        <li>APIs</li>
+        <li>Node</li>
+        <li>Express</li>
+        <li>MySql, Sequelize</li>
+        <li>Restful API</li>
+        <li>Graphql</li>
+      </ul>
+      <a
+        className="button is-primary"
+        href="/GordonJackmanResume.pdf"
+        download="GordonJackmanResume.pdf"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <span className="icon">
+          <i className="fas fa-download"></i>
+        </span>
+        <span className='download'>Download My Resume</span>
+      </a>
+    </Styles>
   );
 }
 
